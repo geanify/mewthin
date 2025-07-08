1f5762bd9227f9b399e721fb47961ffa243dee8a
geanify <geanify@gmail.com>
2025-07-08 16:43:33 +0300

    fix

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+if (!window.socket) {
+  window.socket = io('http://localhost:21234');
+}
 
-export default socket; 
+export default window.socket; 
