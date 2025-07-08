a499737e211ff39727d0bc466fabec6c9d85ab05
geanify <geanify@gmail.com>
2025-07-08 18:00:36 +0300

    fixed movement

--- client/src/InputHandler.js
+++ client/src/InputHandler.js
@@ -19,4 +19,32 @@ export default class InputHandler {
       down: this.cursors.down.isDown || this.wasd.down.isDown
     };
   }
+
+  update(entity, scene) {
+    // Only update if this is the local player
+    if (!entity.isPlayer || entity.id !== scene.playerId) return;
+    const speed = (entity.stats && entity.stats.movementSpeed) ? entity.stats.movementSpeed : 2;
+    const dir = this.getDirection();
+    let moved = false;
+    if (dir.left) {
+      entity.x -= speed;
+      moved = true;
+    }
+    if (dir.right) {
+      entity.x += speed;
+      moved = true;
+    }
+    if (dir.up) {
+      entity.y -= speed;
+      moved = true;
+    }
+    if (dir.down) {
+      entity.y += speed;
+      moved = true;
+    }
+    if (moved && typeof window !== 'undefined' && window.socket) {
+      window.socket.emit('move', { x: entity.x, y: entity.y });
+      if (scene.drawEntities) scene.drawEntities();
+    }
+  }
 } 
